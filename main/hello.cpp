9d8b21eab6108c2a4ac39cd2cdb254dfb7f72c3b
Pallavipatil55 <pallavipatila2@gmail.com>
2025-04-12 15:05:48 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -6,6 +6,6 @@ int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run this  \n";
-  cout<<"Pallavi M Patil";
+  cout<<"Pallavi M Patil\n"
   return 0;
 }
